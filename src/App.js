4ed5cd575eee8bbb84e775f03061de947f91fe71
ritik9694 <ritikkumarbhai@gmail.com>
2023-05-19 15:32:14 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+  return(
+    <>
+         <Navbar/>   
+        <Route exact path="/"> 
+           <Home/>
+          
+          </Route> 
+
+
+        <Route path="/about"> 
+           <About/>
+          
+          </Route> 
+
+
+        <Route path="/contact"> 
+           <Contact/>
+          
+          </Route> 
+
+        <Route path="/login"> 
+           <Login/>
+          
+          </Route> 
+
+
+        <Route path="/signup"> 
+           <Signup/>
+          
+          </Route> 
+           
+    </>
+  )
+}  
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Switch , Route} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+ReactDOM.render(
+     <React.StrictMode>
+  
+    <BrowserRouter>
+    <Switch>
+<Route exact path ="/" component ={App} />
+</Switch>
+    </BrowserRouter>
+    </React.StrictMode>,
+  
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
